bbe70e913ea48441a82e4a6496fac20acba59f88
José Ithalo <jose_ithalo@hotmail.com>
2024-05-03 12:49:27 -0300

    Importação do componente footer

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,12 +1,14 @@
 import "../css/global.css";
 
 import Header from "../components/Header/header";
+import Footer from "../components/Footer/footer";
 
 function Home() {
     return (
         <div className="containerHome">
             <Header></Header>
             <h1>Projeto</h1>
+            <Footer></Footer>
         </div>
     )
 }
