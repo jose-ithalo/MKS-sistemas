1a98d33a945f19210854721d9d08eb41c0549ee7
José Ithalo <jose_ithalo@hotmail.com>
2024-05-04 20:38:41 -0300

    Area central de produtos criada

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -7,10 +7,17 @@ function Home() {
     return (
         <div className="containerHome">
             <Header></Header>
-            <h1>Projeto</h1>
+            <div className="productsArea">
+                <div className="card">
+                    <h1>Projeto</h1>
+                    <div className="card__price">
+                        <span>COMPRAR</span>
+                    </div>
+                </div>
+            </div>
             <Footer></Footer>
         </div>
-    )
+    );
 }
 
 export default Home; 
