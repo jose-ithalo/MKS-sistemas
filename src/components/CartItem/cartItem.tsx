aa0524850cbbda70e5080f8dfa80212426e14007
José Ithalo <jose_ithalo@hotmail.com>
2024-05-10 15:36:01 -0300

    Conteúdo do item criado

--- src/components/CartItem/cartItem.tsx
+++ src/components/CartItem/cartItem.tsx
@@ -1,4 +1,5 @@
 import closeIcon from "../../assets/x-icon.svg";
+import appWatch from "../../assets/apple-watch.png";
 
 function CartItem() {
     return (
@@ -6,7 +7,9 @@ function CartItem() {
             <button>
                 <img src={closeIcon} alt="Fechar" />
             </button>
-            <h1>Item</h1>
+            <img src={appWatch} alt="Produto" />
+            <p>Apple Watch Series 4 GPS</p>
+            <h2>R$399</h2>
         </div>
     );
 }
