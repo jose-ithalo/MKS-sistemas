fb782c585599335d84cf1ceace60adcf72aa7df8
José Ithalo <jose_ithalo@hotmail.com>
2024-05-29 11:24:56 -0300

    Integração com a API realizada

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -5,11 +5,27 @@ import Header from "../components/Header/header";
 import Card from "../components/Card/card";
 import Footer from "../components/Footer/footer";
 
+import { useQuery } from "react-query";
+import axios from "axios";
 import { motion } from "framer-motion";
 
 const itemsList: string[] = ['Watch', "Watch2", "Watch3", "Watch4", "Watch5", "Watch6", "Watch7", "Watch8"];
 
 function Home() {
+
+    const urlApi = "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC";
+
+    const { data, isLoading } = useQuery("items", async () => {
+        const response = await axios.get(`${urlApi}`);
+        return response.data.products;
+    });
+
+    if (isLoading) {
+        return <div>Carregando...</div>;
+    }
+
+    console.log(data);
+
     return (
         <FileProvider>
             <div className="containerHome">
