9f27ba7dd06cf5691c14b3ad0981481b750fa014
José Ithalo <jose_ithalo@hotmail.com>
2024-06-21 14:09:47 -0300

    Novo estado adicionado

--- src/hooks/useListProvider.ts
+++ src/hooks/useListProvider.ts
@@ -4,10 +4,13 @@ import ListType from "../types/listType";
 
 function useListProvider() {
     const [cartList, setCartList] = useState<ListType[]>([]);
+    const [openSnack, setOpenSnack] = useState<boolean>(false);
 
     return {
         cartList,
-        setCartList
+        setCartList,
+        openSnack,
+        setOpenSnack
     }
 }
 
